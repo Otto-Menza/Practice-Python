396ba990115fa9eb697e5a4a0e8a0df55d649743
otto-menza <otoniel.jump@gmail.com>
2026-09-18 15:24:36 -0500

    cicle for and while, basic concept

---
+++ Basic Concepts/ciclos/ciclo_for.py
@@ -0,0 +1,13 @@
+print('*** Ciclo For ***')
+cadena = 'Hola Mundo'
+for letra in cadena:
+    print(letra, end=' ')
+print('\n')
+#para cpntar usamos la funsion range para el for, ya que el for itera sobre un pbjeto o palabra.
+for contador in range(1,6):
+    print(contador, end=' ')
+
+#Iterar lista:
+frutas=['platano','pera','manzana']
+for fruta in frutas:
+    print(fruta, end=' ')
---
+++ Basic Concepts/ciclos/ciclo_while.py
@@ -0,0 +1,8 @@
+print('*** Ciclo While ***')
+
+contador=1
+while contador <= 5:
+    print('Contador: ',contador, end=' - ')
+    contador +=1
+print('\n')
+print('Fin del while')
